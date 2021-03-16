6d8211b393c98bad92e9d8fe9d4460e7784960b0
Adnan Joraid <adnanjoraidca@gmail.com>
2021-03-16 16:04:27 -0400

    added authentication_service class under the services folder that will be responsible for handling the signin option in addtion to configuring the project on firebase cloud

---
+++ lib/services/authenticationservice.dart
@@ -0,0 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthenticationService {
+  final FirebaseAuth _firebaseAuth;
+  AuthenticationService(this._firebaseAuth);
+
+  Stream<User> get authStateChange => _firebaseAuth.authStateChanges();
+
+  Future<String> signIn(String email, String password) async {
+    try {
+      await _firebaseAuth.signInWithEmailAndPassword(
+          email: email, password: password);
+      return "Signed in";
+    } on FirebaseAuthException catch (e) {
+      return e.message;
+    }
+  }
+}
