805b8774ea0bece028417e111dc5d31bdfd67654
Adnan Joraid <adnanjoraidca@gmail.com>
2021-03-15 20:26:33 -0400

    Added signpage view and a controller folder that has a route class which is responsible for handling the navigation between pages.

---
+++ lib/controller/route.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:healthkick/main.dart';
+import 'package:healthkick/screens/loginscreen.dart';
+
+/*
+ this class will be resonsible for handling the screen navigation of the application. It will also check if the passed values to this page is correct by 
+ checking if the type passed is the one desired. If values are not correct, it will display an error page <Widget>. 
+ */
+class RouteGenerator {
+  static Route<dynamic> generatedRoute(RouteSettings settings) {
+    final args =
+        settings.arguments; //This variable will hold the name of the page.
+
+    //checks the name of the page and navigates according to it.
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (_) => MyHomePage());
+
+      case '/login':
+        return MaterialPageRoute(builder: (_) => LoginPage());
+
+      default: //if there is no route with the name specified, return an error page
+        return _routeNotFound();
+    }
+  }
+
+  static Route<dynamic> _routeNotFound() {
+    return MaterialPageRoute(builder: (_) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text("ERROR, PAGE NOT FOUND"),
+        ),
+      );
+    });
+  }
+}
---
+++ lib/screens/loginscreen.dart
@@ -0,0 +1,22 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:healthkick/main.dart';
+
+void main() => runApp(MyApp());
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login Page"),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[Text("Loginpage test")],
+        ),
+      ),
+    );
+  }
+}
