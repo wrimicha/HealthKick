a8b012ad6257a69837040bd0135920e5803786b5
Adnan Joraid <adnanjoraidca@gmail.com>
2021-03-27 23:20:17 -0400

    commented authentication service class

--- lib/services/authenticationservice.dart
+++ lib/services/authenticationservice.dart
@@ -1,18 +1,18 @@
-import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
 
-class AuthenticationService {
-  final FirebaseAuth _firebaseAuth;
-  AuthenticationService(this._firebaseAuth);
+// class AuthenticationService {
+//   final FirebaseAuth _firebaseAuth;
+//   AuthenticationService(this._firebaseAuth);
 
-  Stream<User> get authStateChanges => _firebaseAuth.authStateChanges();
+//   Stream<User> get authStateChanges => _firebaseAuth.authStateChanges();
 
-  Future<String> signIn(String email, String password) async {
-    try {
-      await _firebaseAuth.signInWithEmailAndPassword(
-          email: email, password: password);
-      return "Signed in";
-    } on FirebaseAuthException catch (e) {
-      return e.message;
-    }
-  }
-}
+//   Future<String> signIn(String email, String password) async {
+//     try {
+//       await _firebaseAuth.signInWithEmailAndPassword(
+//           email: email, password: password);
+//       return "Signed in";
+//     } on FirebaseAuthException catch (e) {
+//       return e.message;
+//     }
+//   }
+// }
