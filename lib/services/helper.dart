5e5dc5112cc0049bb8060b14d4efcd07c6350462
Adnan Joraid <adnanjoraidca@gmail.com>
2021-04-02 18:52:01 -0400

    created helper class that will save users email and password in local storage using sharedPreference

--- lib/services/helper.dart
+++ lib/services/helper.dart
@@ -1,1 +1,27 @@
-class Helper {}
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Helper {
+  static String sharedPreferenceUserName = "USERNAME";
+  static String sharedPreferenceUserEmail = "USEREMAIL";
+
+  static Future<void> saveUserName(String username) async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    return await preferences.setString(sharedPreferenceUserName, username);
+  }
+
+  static Future<void> saveUserEmail(String email) async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    return await preferences.setString(sharedPreferenceUserEmail, email);
+  }
+
+  //getting the data from shared preference
+  static Future<String> getUserName() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    return await preferences.getString(sharedPreferenceUserName);
+  }
+
+  static Future<String> getUserEmail() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    return await preferences.getString(sharedPreferenceUserEmail);
+  }
+}
