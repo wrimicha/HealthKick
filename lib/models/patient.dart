f45d6a5f5eb93af841dc033fe0a6bfa804675c97
Adnan Joraid <adnanjoraidca@gmail.com>
2021-04-02 13:36:03 -0400

    added signin method to authentication service class

--- lib/models/patient.dart
+++ lib/models/patient.dart
@@ -1,10 +1,4 @@
 class Patient {
-  int uid;
-  String firstName;
-  String lastName;
-  String email;
-  // String sex;
-  // double height;
-  // double weight;
-  // String profileImageUrl;
+  final String uid;
+  Patient({this.uid});
 }
--- lib/services/authenticationservice.dart
+++ lib/services/authenticationservice.dart
@@ -1,4 +1,23 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:healthkick/models/patient.dart';
 
-class AuthenticationServices {}
+class AuthenticationServices {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Patient _firebaseUser(User user) {
+    return user != null ? Patient(uid: user.uid) : null;
+  }
+
+  Future signInWithEmailAndPass(String email, String password) async {
+    try {
+      var result = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      User user = result.user;
+      return _firebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+}
