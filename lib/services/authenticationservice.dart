b2fc6c959cfd2eb414f44eabf18f65464b466b02
Adnan Joraid <adnanjoraidca@gmail.com>
2021-04-02 13:29:23 -0400

    added database and authentication services classes

--- lib/services/authenticationservice.dart
+++ lib/services/authenticationservice.dart
@@ -1,21 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-class DatabaseManager {
-  final CollectionReference usersRef =
-      FirebaseFirestore.instance.collection('users');
-
-  Future getUserData() async {
-    try {
-      List items = [];
-      await usersRef.get().then((querySnapshot) {
-        querySnapshot.docs.forEach((element) {
-          items.add(element.data);
-        });
-      });
-    } catch (e) {
-      print(e.toString());
-      return null;
-    }
-  }
-}
+class AuthenticationServices {}
