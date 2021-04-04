081c6987b65f846d769e163dd2f6c84f070bafae
Adnan Joraid <adnanjoraidca@gmail.com>
2021-04-04 18:50:18 -0400

    refactoring search.dart

--- lib/screens/searchscreen.dart
+++ lib/screens/searchscreen.dart
@@ -1,11 +1,33 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:healthkick/services/database.dart';
+import 'package:flutter/material.dart';
 
-class SearchScreen extends StatelessWidget {
+class Search extends StatefulWidget {
+  @override
+  SearchScreen createState() => SearchScreen();
+}
+
+class SearchScreen extends State<Search> {
   DatabaseManager manager = new DatabaseManager();
+  TextEditingController searchController = new TextEditingController();
   QuerySnapshot usersListSnapshot;
+
+  bool isLoading = false;
+  bool isSearching = false;
+
+  initSearch() async {
+    if (searchController.text.isNotEmpty) {
+      setState(() {});
+    }
+  }
 
   @override
   Widget build(BuildContext context) {}
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
 }
