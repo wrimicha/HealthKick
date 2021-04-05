b09ba0590000b4ee5490e8deab969060086fed5d
Adnan Joraid <adnanjoraidca@gmail.com>
2021-04-05 02:54:06 -0400

    finished implementing the chat home screen for doctor

--- lib/screens/chathomescreen_doctor.dart
+++ lib/screens/chathomescreen_doctor.dart
@@ -1,6 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:healthkick/screens/chatroomscreen.dart';
+import 'package:healthkick/screens/searchscreen.dart';
 import 'package:healthkick/services/authenticationservice.dart';
 import 'package:healthkick/services/database.dart';
 
@@ -11,8 +14,149 @@ class ChatDoctor extends StatefulWidget {
 
 class ChatHomeDoctor extends State<ChatDoctor> {
   DatabaseManager manager = new DatabaseManager();
-  QuerySnapshot usersListSnapshot;
+  Stream chatRooms;
+  var username;
+  //todo: factoring this method -- added it in databaseManager class to avoid repetetion.
+  String getUserNameFromDb() {
+    User user = FirebaseAuth.instance.currentUser;
+    var userQuery = FirebaseFirestore.instance
+        .collection('users')
+        .doc(user.uid)
+        .get()
+        .then((value) {
+      username = value.data()['name'];
+      print(username);
+    });
+    return username;
+  }
+
+  void _signOut(BuildContext context) {
+    AuthenticationServices()
+        .signOut()
+        .then((_) => Navigator.of(context).pushNamed('/loginscreen'));
+  }
+
+  Widget chatRoomsList() {
+    return StreamBuilder(
+      stream: chatRooms,
+      builder: (context, snapshot) {
+        return snapshot.hasData
+            ? ListView.builder(
+                itemCount: snapshot.data.docs.length,
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  return ChatRoomsTile(
+                    userName: snapshot.data.docs[index]
+                        .data()['chatRoomId']
+                        .toString()
+                        .replaceAll("_", "")
+                        .replaceAll(getUserNameFromDb(), ""),
+                    chatRoomId: snapshot.data.docs[index].data()['chatRoomId'],
+                  );
+                },
+              )
+            : Container();
+      },
+    );
+  }
 
   @override
-  Widget build(BuildContext context) {}
+  void initState() {
+    getUserInfoChats();
+    super.initState();
+  }
+
+  getUserInfoChats() async {
+    await DatabaseManager().getUserChats(username).then((snapshot) {
+      setState(() {
+        chatRooms = snapshot;
+        print("we got the data + ${chatRooms.toString()} name is $username");
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Chat Page/doctor-alpha v"),
+        actions: [
+          Center(
+              child: Text(
+            "Logout",
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.normal),
+          )),
+          IconButton(
+              icon: Icon(Icons.logout),
+              onPressed: () {
+                _signOut(context);
+              })
+        ],
+        backgroundColor: Colors.pink[600],
+        automaticallyImplyLeading: false,
+      ),
+      body: Container(
+        child: chatRoomsList(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.search),
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => Search()));
+        },
+      ),
+    );
+  }
+}
+
+class ChatRoomsTile extends StatelessWidget {
+  final String userName;
+  final String chatRoomId;
+
+  ChatRoomsTile({this.userName, @required this.chatRoomId});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => ChatRoom(
+                      chatRoomId: chatRoomId,
+                    )));
+      },
+      child: Container(
+        color: Colors.white,
+        padding: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+        child: Row(
+          children: [
+            Container(
+              height: 30,
+              width: 30,
+              decoration: BoxDecoration(
+                  color: Colors.blue, borderRadius: BorderRadius.circular(30)),
+              child: Text(userName.substring(0, 1),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontFamily: 'OverpassRegular',
+                      fontWeight: FontWeight.w300)),
+            ),
+            SizedBox(
+              width: 12,
+            ),
+            Text(userName,
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontFamily: 'OverpassRegular',
+                    fontWeight: FontWeight.w300))
+          ],
+        ),
+      ),
+    );
+  }
 }
