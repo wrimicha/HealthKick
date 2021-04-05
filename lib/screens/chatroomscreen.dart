f150554b5dd4ba1c15522e6796d78d948dea090c
Adnan Joraid <adnanjoraidca@gmail.com>
2021-04-05 14:19:09 -0400

    added chat page design and functionlalites

--- lib/screens/chatroomscreen.dart
+++ lib/screens/chatroomscreen.dart
@@ -1,4 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:healthkick/services/database.dart';
+import 'package:healthkick/widget/widget.dart';
 
 class Chat extends StatefulWidget {
   final String chatRoomId;
@@ -10,6 +15,173 @@ class Chat extends StatefulWidget {
 }
 
 class ChatRoom extends State<Chat> {
+  Stream<QuerySnapshot> chats;
+  TextEditingController messageController = new TextEditingController();
+  var username;
+  //todo: factoring this method -- added it in databaseManager class to avoid repetetion.
+  // ignore: missing_return
+  Future<void> getUserNameFromDb() {
+    User user = FirebaseAuth.instance.currentUser;
+    var userQuery = FirebaseFirestore.instance
+        .collection('users')
+        .doc(user.uid)
+        .get()
+        .then((value) {
+      username = value.data()['name'].toString();
+      print(username);
+    });
+  }
+
+  Widget chatMessages() {
+    return StreamBuilder(
+      stream: chats,
+      builder: (context, snapshot) {
+        return snapshot.hasData
+            ? ListView.builder(
+                itemCount: snapshot.data.docs.length,
+                itemBuilder: (context, index) {
+                  return MessageTile(
+                    message: snapshot.data.docs[index].data()['message'],
+                    sendByMe:
+                        username == snapshot.data.docs[index].data()['sendBy'],
+                  );
+                },
+              )
+            : Container();
+      },
+    );
+  }
+
+  addMessage() {
+    var name = getUserNameFromDb();
+    if (messageController.text.isNotEmpty) {
+      Map<String, dynamic> chatMessageMap = {
+        "sendBy": username,
+        "message": messageController.text,
+        "time": DateTime.now().microsecondsSinceEpoch,
+      };
+
+      DatabaseManager().addMessage(widget.chatRoomId, chatMessageMap);
+
+      setState(() {
+        messageController.text = "";
+      });
+      super.initState();
+    }
+  }
+
   @override
-  Widget build(BuildContext context) {}
+  void initState() {
+    DatabaseManager().getChats(widget.chatRoomId).then((val) {
+      setState(() {
+        chats = val;
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBarMain(context),
+      body: Container(
+        child: Stack(
+          children: [
+            chatMessages(),
+            Container(
+              alignment: Alignment.bottomCenter,
+              width: MediaQuery.of(context).size.width,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                color: Colors.grey[400],
+                child: Row(
+                  children: [
+                    Expanded(
+                        child: TextField(
+                      controller: messageController,
+                      style: simpleTextStyle(),
+                      decoration: InputDecoration(
+                          hintText: "Write a message..",
+                          hintStyle:
+                              TextStyle(color: Colors.black, fontSize: 16),
+                          border: InputBorder.none),
+                    )),
+                    SizedBox(
+                      width: 16,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        addMessage();
+                      },
+                      child: Container(
+                        height: 40,
+                        width: 40,
+                        decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                                colors: [
+                                  const Color(0x36FFFFFF),
+                                  const Color(0x0FFFFFFF),
+                                ],
+                                begin: FractionalOffset.topLeft,
+                                end: FractionalOffset.bottomRight),
+                            borderRadius: BorderRadius.circular(40)),
+                        padding: EdgeInsets.all(12),
+                        child: Icon(
+                          Icons.send,
+                          size: 25,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MessageTile extends StatelessWidget {
+  final String message;
+  final bool sendByMe;
+
+  MessageTile({@required this.message, @required this.sendByMe});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(
+          top: 8, bottom: 8, left: sendByMe ? 0 : 24, right: sendByMe ? 24 : 0),
+      alignment: sendByMe ? Alignment.centerRight : Alignment.centerLeft,
+      child: Container(
+        margin:
+            sendByMe ? EdgeInsets.only(left: 30) : EdgeInsets.only(right: 30),
+        padding: EdgeInsets.only(top: 17, bottom: 17, left: 20, right: 20),
+        decoration: BoxDecoration(
+            borderRadius: sendByMe
+                ? BorderRadius.only(
+                    topLeft: Radius.circular(23),
+                    topRight: Radius.circular(23),
+                    bottomLeft: Radius.circular(23))
+                : BorderRadius.only(
+                    topLeft: Radius.circular(23),
+                    topRight: Radius.circular(23),
+                    bottomRight: Radius.circular(23)),
+            gradient: LinearGradient(
+              colors: sendByMe
+                  ? [const Color(0xff007EF4), const Color(0xff2A75BC)]
+                  : [const Color(0x1AFFFFFF), const Color(0x1AFFFFFF)],
+            )),
+        child: Text(message,
+            textAlign: TextAlign.start,
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: 16,
+                fontFamily: 'OverpassRegular',
+                fontWeight: FontWeight.w300)),
+      ),
+    );
+  }
 }
