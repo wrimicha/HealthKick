487fa8213b5c97406d3ddf6f983de304814efd17
Michael Wright <media.mikewright@gmail.com>
2021-04-08 01:54:51 -0400

    Added google maps to the main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:healthkick/controller/route.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'dart:async';
 
 Future<void> main() async {
   //initializing firebase app.
@@ -35,29 +37,29 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  Completer<GoogleMapController> _controller = Completer();
+
+  static const LatLng _center = const LatLng(43.6532, -79.3832);
+
+  void _onMapCreated(GoogleMapController controller) {
+    _controller.complete(controller);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:
-          AppBar(title: Text(widget.title), backgroundColor: Colors.pink[600]),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Main Page, Click login to start',
-            ),
-          ],
+        appBar: AppBar(
+          title: Text('Maps Sample App'),
+          backgroundColor: Colors.red[400],
         ),
-      ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 13.0,
+          ),
+        ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
           Navigator.of(context).pushNamed('/loginscreen');
