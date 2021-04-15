7815dfd475a23cf5e233bfd43824f9c17a719d97
Adnan Joraid <adnanjoraidca@gmail.com>
2021-04-15 16:30:45 -0400

    added a functon in the database manager class that lets doctors book appointments for patients by providing an email and a date

--- lib/screens/appointments.dart
+++ lib/screens/appointments.dart
@@ -1,11 +1,78 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-
+import 'package:healthkick/models/constants.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:healthkick/services/database.dart';
 
 //doctors appointment form.... will use database to set data for user using users email and password (maybe use the search class).
 class AppointmentPage extends StatefulWidget {
   AppointmentScreen createState() => AppointmentScreen();
+}
+
+convertNumToDay(String day) {
+  switch (day) {
+    case '1':
+      return 'Mon';
+    case '2':
+      return 'Tues';
+    case '3':
+      return 'Wed';
+    case '4':
+      return 'Thurs';
+    case '5':
+      return 'Fri';
+    case '6':
+      return 'Sat';
+    case '7':
+      return 'Sun';
+  }
+}
+
+numToOrdinal(int num) {
+  switch (num) {
+    case 1:
+    case 31:
+    case 21:
+      return '${num}st';
+    case 2:
+    case 22:
+      return '${num}nd';
+    case 3:
+    case 23:
+      return '${num}rd';
+    default:
+      return '${num}th';
+  }
+}
+
+convertNumToMonth(String month) {
+  switch (month) {
+    case '1':
+      return 'January';
+    case '2':
+      return 'Feburary';
+    case '3':
+      return 'March';
+    case '4':
+      return 'April';
+    case '5':
+      return 'May';
+    case '6':
+      return 'June';
+    case '7':
+      return 'July';
+    case '8':
+      return 'August';
+    case '9':
+      return 'September';
+    case '10':
+      return 'October';
+    case '11':
+      return 'November';
+    case '12':
+      return 'December';
+  }
 }
 
 class AppointmentScreen extends State<AppointmentPage> {
@@ -25,7 +92,7 @@ class AppointmentScreen extends State<AppointmentPage> {
       setState(() {
         selectedDate = picked;
         var date =
-            "${picked.toLocal().day}/${picked.toLocal().month}/${picked.toLocal().year}";
+            "${convertNumToDay(picked.weekday.toString())}. ${convertNumToMonth(picked.month.toString())} ${numToOrdinal(picked.day)}, With ${Constants.userName}";
         _dateController.text = date;
       });
     }
@@ -57,7 +124,7 @@ class AppointmentScreen extends State<AppointmentPage> {
                       onSaved: (newValue) => appointmentDate = newValue,
                       controller: _dateController,
                       decoration: InputDecoration(
-                          labelText: "Appoitnment Date",
+                          labelText: "Appointment Date",
                           icon: Icon(Icons.calendar_today)),
                       validator: (value) {
                         if (value.isEmpty) return "Please add a patient date";
@@ -73,7 +140,8 @@ class AppointmentScreen extends State<AppointmentPage> {
                   onPressed: () {
                     if (_formKey.currentState.validate()) {
                       _formKey.currentState.save();
-                      print(patientEmail);
+                      DatabaseManager()
+                          .getPatientId(patientEmail, appointmentDate);
                     }
                   },
                 )
