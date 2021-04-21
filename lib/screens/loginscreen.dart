f9f1811ed7b6ebfd1a15767e5936e45099d566a8
Adnan Joraid <adnanjoraidca@gmail.com>
2021-04-21 02:07:07 -0400

    removing unused comments

--- lib/screens/loginscreen.dart
+++ lib/screens/loginscreen.dart
@@ -12,10 +12,6 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:healthkick/services/database.dart';
 import 'package:healthkick/services/helper.dart';
 
-// void main() {
-//   runApp(MyApp());
-// }
-
 class LoginPage extends StatelessWidget {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   String _email, _password;
@@ -24,125 +20,123 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: Form(
-          key: _formKey,
-          child: SingleChildScrollView(
-              child: Column(
-                children: <Widget>[
-                  SizedBox(height: 80.0),
-
-                  Container(
-                      margin: EdgeInsetsDirectional.fromSTEB(18.0, 0, 18.0, 0),
-                      //padding: EdgeInsets.all(20),
-                      child: Column(
-                        children: <Widget>[
-                          Text(
-                            'Sign In',
-                            style: TextStyle(
-                              fontSize: 32.0,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.red[500],
-                            ),
+        body: Form(
+      key: _formKey,
+      child: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 80.0),
+            Container(
+                margin: EdgeInsetsDirectional.fromSTEB(18.0, 0, 18.0, 0),
+                //padding: EdgeInsets.all(20),
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      'Sign In',
+                      style: TextStyle(
+                        fontSize: 32.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.red[500],
+                      ),
+                    ),
+                    SizedBox(height: 20.0),
+                    Text(
+                      'Please sign in with your email registered at Northgate Medical Doctors Office',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        color: Colors.black38,
+                      ),
+                    ),
+                    SizedBox(height: 40.0),
+                    TextFormField(
+                      // ignore: missing_return
+                      validator: (input) {
+                        if (input.isEmpty) {
+                          return 'Provide an email';
+                        }
+                      },
+                      decoration: InputDecoration(
+                        labelText: "Enter Email",
+                        fillColor: Colors.white,
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: BorderSide(
+                            color: Colors.blue,
                           ),
-                          SizedBox(height: 20.0),
-                          Text(
-                            'Please sign in with your email registered at Northgate Medical Doctors Office',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontSize: 14.0,
-                              color: Colors.black38,
-                            ),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                          borderSide: BorderSide(
+                            width: 1.5,
+                            color: Colors.grey[500],
                           ),
-                          SizedBox(height: 40.0),
-                          TextFormField(
-                            // ignore: missing_return
-                            validator: (input) {
-                              if (input.isEmpty) {
-                                return 'Provide an email';
-                              }
-                            },
-                            decoration: InputDecoration(
-                              labelText: "Enter Email",
-                              fillColor: Colors.white,
-                              focusedBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10.0),
-                                borderSide: BorderSide(
-                                  color: Colors.blue,
-                                ),
-                              ),
-                              enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(18.0),
-                                borderSide: BorderSide(
-                                  width: 1.5,
-                                  color: Colors.grey[500],
-                                ),
-                              ),
-                            ),
-                            onSaved: (input) => _email = input,
+                        ),
+                      ),
+                      onSaved: (input) => _email = input,
+                    ),
+                    SizedBox(height: 40.0),
+                    TextFormField(
+                      // ignore: missing_return
+                      validator: (input) {
+                        if (input.length < 6) {
+                          return 'Longer password please';
+                        }
+                      },
+                      decoration: InputDecoration(
+                        labelText: "Enter Password",
+                        fillColor: Colors.white,
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: BorderSide(
+                            color: Colors.blue,
                           ),
-                          SizedBox(height: 40.0),
-                          TextFormField(
-                            // ignore: missing_return
-                            validator: (input) {
-                              if (input.length < 6) {
-                                return 'Longer password please';
-                              }
-                            },
-                            decoration: InputDecoration(
-                              labelText: "Enter Password",
-                              fillColor: Colors.white,
-                              focusedBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10.0),
-                                borderSide: BorderSide(
-                                  color: Colors.blue,
-                                ),
-                              ),
-                              enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(18.0),
-                                borderSide: BorderSide(
-                                  width: 1.5,
-                                  color: Colors.grey[500],
-                                ),
-                              ),
-                            ),
-                            onSaved: (input) => _password = input,
-                            obscureText: true,
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                          borderSide: BorderSide(
+                            width: 1.5,
+                            color: Colors.grey[500],
                           ),
-                          SizedBox(height: 70.0),
-                          SizedBox(
-                            height: 60.0,
-                            width: double.infinity, // <-- match_parent
-                            child: ElevatedButton(
-                              style: ButtonStyle(
-                                backgroundColor: MaterialStateProperty.all<Color>(Colors.red[400]),
-                                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                                  RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(14.0),
-                                      side: BorderSide(color: Colors.red)
-                                  ),
-                                ),
-                              ),
-                              child: Text(
-                                'Sign In',
-                                style: TextStyle(
-                                  fontSize: 22.0,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              onPressed: () {
-                                signIn(context);
-                              },
-                            ),
+                        ),
+                      ),
+                      onSaved: (input) => _password = input,
+                      obscureText: true,
+                    ),
+                    SizedBox(height: 70.0),
+                    SizedBox(
+                      height: 60.0,
+                      width: double.infinity, // <-- match_parent
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(Colors.red[400]),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(14.0),
+                                side: BorderSide(color: Colors.red)),
                           ),
-                        ],
-                      )
-                  ),
-                ],
-              ),
-            ),
-          )
-    );
+                        ),
+                        child: Text(
+                          'Sign In',
+                          style: TextStyle(
+                            fontSize: 22.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        onPressed: () {
+                          signIn(context);
+                        },
+                      ),
+                    ),
+                  ],
+                )),
+          ],
+        ),
+      ),
+    ));
   }
 
   void signIn(BuildContext context) async {
@@ -169,17 +163,12 @@ class LoginPage extends StatelessWidget {
 
               if (value.data()['type'] == "doctor") {
                 print(Constants.userName);
-                //Navigator.of(context)
-                // .pushNamed('/chathomescreen_doctor', arguments: user);
+
                 Navigator.of(context).pushNamed('/homescreendoctor');
               }
               if (value.data()['type'] != "doctor")
                 Navigator.of(context).pushNamed('/homescreen');
             });
-
-            //for testing, ill navigate users to chat screen (instead of homescreen)
-
-            //Navigator.of(context).pushNamed('/homescreen', arguments: user);
           }
         });
       } catch (e) {
