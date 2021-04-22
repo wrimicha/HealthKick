78ff872778ceca1aa182abf8a90841af199e2968
Michael Wright <wrimicha@sheridancollege.ca>
2021-04-22 19:52:05 -0400

    Updated the Doctor homepage

--- lib/screens/homescreendoctor.dart
+++ lib/screens/homescreendoctor.dart
@@ -20,27 +20,132 @@ class HomePageDoctor extends StatelessWidget {
         automaticallyImplyLeading: false,
       ),
       body: Container(
+          margin: EdgeInsetsDirectional.fromSTEB(18.0, 60, 18.0, 0),
           child: Column(
         children: [
-          Text("Hello " + Constants.userName),
-          RaisedButton(
-            child: Text("Go to chat page"),
-            onPressed: () {
-              Navigator.of(context).pushNamed('/chathomescreen_doctor');
-            },
+          SizedBox(
+            width: double.infinity,
+            child: Container(
+              margin: EdgeInsetsDirectional.fromSTEB(18.0, 0, 0, 15),
+              child: Text(
+                "Hello " + Constants.userName + ",",
+                textAlign: TextAlign.left,
+                style: TextStyle(fontSize: 22.0,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
           ),
-          RaisedButton(
-            child: Text("Create Patient Appointments"),
-            onPressed: () {
-              Navigator.of(context).pushNamed('/appointmentscreen');
-            },
+          SizedBox(
+            height: 20,
           ),
-          RaisedButton(
-            child: Text("Patient's Health Form"),
-            onPressed: () {
-              Navigator.of(context).pushNamed('/healthformscreen');
-            },
-          )
+          SizedBox(
+            height: 60.0,
+            width: double.infinity,
+            child: ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor:
+                MaterialStateProperty.all<Color>(Colors.red[400]),
+                shape:
+                MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(14.0),
+                      side: BorderSide(color: Colors.red)),
+                ),
+              ),
+              child: Text(
+                "Go to chat page",
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                Navigator.of(context).pushNamed('/chathomescreen_doctor');
+              },
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          SizedBox(
+            height: 60.0,
+            width: double.infinity, // <-- match_parent
+            child: ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor:
+                MaterialStateProperty.all<Color>(Colors.red[400]),
+                shape:
+                MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(14.0),
+                      side: BorderSide(color: Colors.red)),
+                ),
+              ),
+              child: Text(
+                "Create Patient Appointments",
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                Navigator.of(context).pushNamed('/appointmentscreen');
+              },
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          SizedBox(
+            height: 60.0,
+            width: double.infinity, // <-- match_parent
+            child: ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor:
+                MaterialStateProperty.all<Color>(Colors.red[400]),
+                shape:
+                MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(14.0),
+                      side: BorderSide(color: Colors.red)),
+                ),
+              ),
+              child: Text(
+                "Patient Health Form",
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                Navigator.of(context).pushNamed('/healthformscreen');
+              },
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          // RaisedButton(
+          //   child: Text("Go to chat page"),
+          //   onPressed: () {
+          //     Navigator.of(context).pushNamed('/chathomescreen_doctor');
+          //   },
+          // ),
+          // RaisedButton(
+          //   child: Text("Create Patient Appointments"),
+          //   onPressed: () {
+          //     Navigator.of(context).pushNamed('/appointmentscreen');
+          //   },
+          // ),
+          // RaisedButton(
+          //   child: Text("Patient's Health Form"),
+          //   onPressed: () {
+          //     Navigator.of(context).pushNamed('/healthformscreen');
+          //   },
+          // )
         ],
       )),
     );
